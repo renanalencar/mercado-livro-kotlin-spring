9436b87a512ee173ce41e70f599ccd70f8cd1b76
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-01-09 23:41:24 -0300

    Criando o primeiro endpoint

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,14 @@
+package com.mercadolivro.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+
+    @GetMapping
+    fun helloWorld(): String {
+        return "Customer 1"
+    }
+
+}
