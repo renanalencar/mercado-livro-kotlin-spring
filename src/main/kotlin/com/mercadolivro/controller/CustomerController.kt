bd0869c6d5bd30ad8733da0dfbafd58b624dd19b
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-01-09 23:47:14 -0300

    Refatorando um pouco

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -12,14 +12,20 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Gustavo", "email@email.com")
+    fun getCustomer(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        val id = if(customers.isEmpty()) {
+            1
+        } else{
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
 
 }
