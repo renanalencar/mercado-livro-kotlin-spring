d549cf934695319a7f74e527d90efca5794fdbf3
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-01-09 23:45:08 -0300

    Implementando o Post Customer

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,9 +1,8 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customer")
@@ -12,6 +11,11 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Gustavo", "email@email.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 
 }
