c5481bf9faee19d4fa2f61e61dba51e1f96857fc
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-01-09 23:53:13 -0300

    Refatorando código

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,11 +1,7 @@
 package com.mercadolivro.service
 
-import com.mercadolivro.controller.request.PostCustomerRequest
-import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.*
 
 @Service
 class CustomerService {
@@ -19,22 +15,24 @@ class CustomerService {
         return customers
     }
 
-    fun create(customer: PostCustomerRequest) {
+    fun create(customer: CustomerModel) {
         val id = if(customers.isEmpty()) {
             1
         } else{
-            customers.last().id.toInt() + 1
+            customers.last().id!!.toInt() + 1
         }.toString()
 
-        customers.add(CustomerModel(id, customer.name, customer.email))
+        customer.id = id
+
+        customers.add(customer)
     }
 
     fun getCustomer(id: String): CustomerModel {
         return customers.filter { it.id == id }.first()
     }
 
-    fun update(id: String, customer: PutCustomerRequest) {
-        customers.filter { it.id == id }.first().let {
+    fun update(customer: CustomerModel) {
+        customers.filter { it.id == customer.id }.first().let {
             it.name = customer.name
             it.email = customer.email
         }
